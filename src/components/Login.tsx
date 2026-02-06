f321cd141b7d9c27228907de366b2cc1a45fc559
mariocesarfilho <mariocesarmoliveirafilho@gmail.com>
2026-02-06 16:50:35 -0300

    Refactor Login component to manage email and password state, and handle form submission

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,13 +1,29 @@
 import { useState } from "react";
+import { postUserByLogin } from "../services/user";
 
 export const Login = () => {
 
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handlePostLogin = async () => {
+        const payload = {
+            email,
+            password
+        }
+        const response = await postUserByLogin(payload);
+
+        if (response.success) {
+            
+        }
+    }
 
     return (
 <div className="max-w-sm mx-auto bg-white p-8 rounded-xl shadow-lg border border-gray-100">
-    <form className="space-y-5">
+    <form className="space-y-5" onSubmit={(e) => {
+        handlePostLogin();
+        e.preventDefault();
+    }}>
         
         <div>
             <label className="block text-sm font-medium text-gray-700 mb-1">
@@ -17,8 +33,9 @@ export const Login = () => {
                 type="email" 
                 value={email}
                 required 
-                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-200"
+                className="w-full px-4 py-2 text-black border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-200"
                 placeholder="seu@email.com"
+                onChange={(e) => setEmail(e.target.value)}
             />
         </div>
 
@@ -30,12 +47,13 @@ export const Login = () => {
                 type="password" 
                 value={password}
                 required
-                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-200"
-                placeholder="••••••••"/>
+                className="w-full px-4 py-2 border text-black border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all duration-200"
+                placeholder="••••••••"
+                onChange={(e) => setPassword(e.target.value)}
+                />
         </div>
 
-        <button>Enviar</button>
-
+        <button type="submit">Enviar</button>
     </form>
 </div>
     );
