34545fa3e039f6aa217b42f438f0bba8a7bd7b86
mariocesarfilho <mariocesarmoliveirafilho@gmail.com>
2026-02-06 16:50:52 -0300

    Refactor postUserByLogin function to include payload parameter and enhance error handling

--- src/services/user/index.tsx
+++ src/services/user/index.tsx
@@ -1,10 +1,30 @@
+import { isAxiosError } from "axios";
 import { api } from "../../configs/api"
-import type { PayloadLoginUserRequest } from "./DTO"
+import type { PayloadLoginUserRequestDTO, PayloadLoginUserResponseDTO, PayloadLoginUserReturnDTO } from "./DTO"
 
-export const postUserByLogin = async (payload: PayloadLoginUserRequest):Promise<> => {
+export const postUserByLogin = async (payload: PayloadLoginUserRequestDTO):Promise<PayloadLoginUserReturnDTO> => {
     try{
-        const response = await api.post("/login");
-    }catch(e){
-        throw e;
+        const response = await api.post<PayloadLoginUserResponseDTO>("/login", payload);
+
+        return {
+            success: true,
+            message: "Login efetuado com sucesso!",
+            data: response.data,
+        };
+    }catch(error){
+        if(isAxiosError(error)) {
+            const backendMessage = 
+                error.response?.data?.error || "Erro ao efetuar login";
+                return {
+                    success: false,
+                    message: backendMessage,
+                    data: null,
+                };
+        }
+        return {
+            success: false,
+            message: "Erro ao efetuar login",
+            data: null
+        }
     }
 };
