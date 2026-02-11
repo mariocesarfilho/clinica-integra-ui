696dd141c6b78c54bc963f721a5e99399b598a93
mariocesarfilho <mariocesarmoliveirafilho@gmail.com>
2026-02-10 23:35:54 -0300

    created file routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Login } from "../pages/login";
+import { Home } from "../pages/home";
+
+export default function Router() {
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/home" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
