02f238de0ec7bf4c8a4e7149343906e8be5a7fc8
mariocesarfilho <mariocesarmoliveirafilho@gmail.com>
2026-02-10 21:23:58 -0300

    Refactor App component to implement routing with react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Login } from './components/Login'
+import { CreateAccount } from './components/CreateAccount'
+import { Home } from './components/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <Login />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/criar-conta" element={<CreateAccount />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
