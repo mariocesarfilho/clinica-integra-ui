5c29a22d73fa1cb25f0f876a5fe34d5b0047fee3
mariocesarfilho <mariocesarmoliveirafilho@gmail.com>
2026-02-10 23:31:32 -0300

    pages login and home

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+
+export const Home: React.FC = () => {
+    return (
+        <div>
+            <h1>Seja Bem Vindo(a)</h1>
+            <button>Anamnese</button>
+            <button>Pacientes</button>
+            <button>Evoluções</button>
+            <button>Relatórios</button>
+        </div>
+    );
+}
